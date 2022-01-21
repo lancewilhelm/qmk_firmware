cec1fe6f10bf064a269ebb1e7d09c7c4281cd270
Lance Wilhelm <lance.wilhelm@gmail.com>
2022-01-20 16:06:13 -0800

    space savings

--- keyboards/crkbd/keymaps/lance/config.h
+++ keyboards/crkbd/keymaps/lance/config.h
@@ -34,6 +34,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 100
 
+// #define NO_ACTION_MACRO
+// #define NO_ACTION_FUNCTION
+
 #ifdef RGBLIGHT_ENABLE
     #undef RGBLED_NUM
     #define RGBLED_NUM 27
